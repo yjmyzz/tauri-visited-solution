c128c781504933813b7588e983ce48f5b784605d
junmingyang <junmingyang@trip.com>
2022-08-15 23:22:17 +0800

    improve perfomance

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React, { useEffect } from 'react';
 import './App.css';
 
+const LOCAL_STORAGE_HISTORY_KEY = "visitedHistory";
+const VISITED_CLASS_NAME = "visited";
+
+/**
+ * 根据localStorage里的历史记录，将a附加上visited样式
+ * by 菩提树下的杨过http://yjmyzz.cnblogs.com/
+ */
 function setVisited() {
-  let localstorageSimuHistory = localStorage.getItem('simuHistory');
+  let localstorageSimuHistory = localStorage.getItem(LOCAL_STORAGE_HISTORY_KEY);
   let simuHistory = localstorageSimuHistory ? JSON.parse(localstorageSimuHistory) : [];
 
   //过期访问记录清理
@@ -18,10 +25,10 @@ function setVisited() {
   }
   if (hasExpired) {
     if (simuHistory.length <= 0) {
-      localStorage.removeItem('simuHistory');
+      localStorage.removeItem(LOCAL_STORAGE_HISTORY_KEY);
     }
     else {
-      localStorage.setItem('simuHistory', JSON.stringify(simuHistory));
+      localStorage.setItem(LOCAL_STORAGE_HISTORY_KEY, JSON.stringify(simuHistory));
     }
   }
 
@@ -29,8 +36,8 @@ function setVisited() {
   let elements = document.getElementsByTagName('a');
   for (let i = 0; i < elements.length; i++) {
     for (let h = 0; h < simuHistory.length; h++) {
-      if (elements[i].href === simuHistory[h].url) {
-        elements[i].className += ' visited';
+      if (elements[i].href === simuHistory[h].url && elements[i].className.indexOf(VISITED_CLASS_NAME) === -1) {
+        elements[i].className += ` ${VISITED_CLASS_NAME}`;
       }
     }
   }
@@ -39,18 +46,21 @@ function setVisited() {
 function bindAddHref() {
   let elements = document.getElementsByTagName('a');
   for (let i = 0; i < elements.length; i++) {
-    elements[i].onclick = (obj) => {
+    elements[i].onclick = () => {
       addHref(elements[i].href);
     }
   }
   setVisited();
 }
 
+/**
+ * a链接点击后将url加入localStorage
+ * @param url 
+ */
 function addHref(url: String) {
-  let localstorageSimuHistory = localStorage.getItem('simuHistory');
+  let localstorageSimuHistory = localStorage.getItem(LOCAL_STORAGE_HISTORY_KEY);
   let simuHistory = localstorageSimuHistory ? JSON.parse(localstorageSimuHistory) : [];
   let found = false;
-  const now = new Date();
   //访问记录过期时间设置（此处仅为示例：30秒）
   const ttl: number = 1000 * 30;
 
@@ -64,18 +74,19 @@ function addHref(url: String) {
     }
   }
   //如果本链接不在访问列表里，则添加
-  if (found === false) {
+  if (!found) {
     //过期时间(示例30秒)
     simuHistory[simuHistory.length] = { "url": url, "expire": new Date().getTime() + ttl };
-    localStorage.setItem('simuHistory', JSON.stringify(simuHistory));
+    localStorage.setItem(LOCAL_STORAGE_HISTORY_KEY, JSON.stringify(simuHistory));
   }
 
+  //此处只是为了方便，把所有点过的a全刷了一把，还可以再优化下（略）
   setVisited();
 }
 
 
 function App() {
-  //在组件加载阶段，触发simuHistory
+  //在组件加载阶段，自动调用bindAddHref
   useEffect(() => {
     return () => {
       console.log("DidMount");
